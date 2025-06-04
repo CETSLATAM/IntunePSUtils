7065d2cf11a76670300fedd75006eb4fa3d69a4a
José P. Cortés <JPCortesP@users.noreply.github.com>
2025-06-04 15:43:17 -0600

    added disclaimer

--- Remediations/WinUpdate/detection.ps1
+++ Remediations/WinUpdate/detection.ps1
@@ -1,3 +1,17 @@
+<#
+.Synopsis
+    Checks the Windows OS version and the last update date.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+#>
+
 # Description: This script checks the Windows OS version and the last update date.
 # It ensures that the OS is at least Windows 10 version 19045 or Windows 11 version 22631.  
 # If the OS version is lower, it outputs the current version and exits with code 1 (Means something to fix).
